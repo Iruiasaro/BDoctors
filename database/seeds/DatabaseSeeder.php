d263c68d409f0fa4d9dff2f2728691b2cac23e0e
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-07-21 00:11:11 +0200

    Fix Bug db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        App\User::truncate();
+        $this->call([
+            UserSeeder::class,
+            SpecializationSeeder::class,
+            SponsorSeeder::class,
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
