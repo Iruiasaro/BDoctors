397bbb7cecccca4650ab3304e292ee6793a439b8
FrancescoPierfederici <francescopierf@gmail.com>
2021-07-22 18:00:54 +0200

    Added vue template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,14 @@
             </label> --}}
             <h1 class="text-white mb-5">
                 Cerca un dottore nella tua zona.
-               
+
             </h1>
             <select class="my-select" name="specializzazione" id="">
                 <option disabled>Scegli una specializzazione</option>
                 @foreach($specializations as $specialization)
-                     <option value="{{$specialization->specs_type}}">{{$specialization->specs_type}}</option>
+                <option value="{{$specialization->specs_type}}">{{$specialization->specs_type}}</option>
                 @endforeach
-               
+
             </select>
             <select class="my-select" name="citta" id="">
                 <option disabled>Scegli una città</option>
@@ -31,21 +31,29 @@
         </form>
     </div>
 </main>
+
 <section class="my-wrap p-5">
     <h2 class="mb-4"> Dottori consigliati </h2>
     <p> Prenota una visita da un dottore consigliato da <span class="color-primary">BDoctors</span> .</p>
     <div class="suggested-doctors d-flex flex-wrap ">
-
         @foreach($doctors as $doctor)
+        <home-doctor-card 
+            img="{{$doctor->image}}"
+            name="{{$doctor->name }}" 
+            link="{{route('doctor.show',$doctor->id)}}">
+
+        </home-doctor-card>
+        @endforeach
+        {{-- @foreach($doctors as $doctor)
         <div class="card" style="">
             <img class="card-img-top" src="{{$doctor->image ? $doctor->image : asset('imgs/avatar.png')}}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">{{$doctor->name}}</h5>
-                <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium deserunt nobis iure quasi debitis magnam.</p>
-                <a href="{{route('doctor.show',$doctor->id)}} " class="btn btn-primary" style="width:100%">Visualizza Profilo</a>
-            </div>
+        <div class="card-body">
+            <h5 class="card-title">{{$doctor->name}}</h5>
+            <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium deserunt nobis iure quasi debitis magnam.</p>
+            <a href="{{route('doctor.show',$doctor->id)}} " class="btn btn-primary" style="width:100%">Visualizza Profilo</a>
         </div>
-        @endforeach
+    </div>
+    @endforeach --}}
     </div>
 </section>
 @endsection
