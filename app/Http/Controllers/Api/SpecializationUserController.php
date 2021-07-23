fd2d4944c2d4531ecae0f8e1dadd9d4be16ad22f
alex-jr-dev <geremia.alessandro@icloud.com>
2021-07-23 15:58:48 +0200

    Api search implemented

--- app/Http/Controllers/Api/SpecializationUserController.php
+++ app/Http/Controllers/Api/SpecializationUserController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\SpecializationUser;
+use App\User;
 
 class SpecializationUserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $specialization_user = SpecializationUser::all();
+        $user_ids = SpecializationUser::where("specialization_id", $request->specialization_id)->get("user_id");
+
+        foreach ($user_ids as $user_id) {
+            $users[] = User::find($user_id);
+        }
 
         return response()->json([
-            "success" => false,
-            "results" => $specialization_user
+            // "success" => false,
+            "results" => $users
         ]);
     }
 }
